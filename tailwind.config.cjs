0fa266c455a2570dec36e0c9b58608ad58ba57e3
thwe thwe tun <thwethwehtun2000@gmail.com>
2023-05-31 18:19:59 +0630

    upload to github

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0158CC",
+        secondary: "#F1F3F4",
+        tertiary: "#14133B",
+      },
+    },
   },
   plugins: [],
 };
