04da74b4b0341081891206c397e0df2491be207e
Raphtalia Kay <miranasiam023@gmail.com>
2023-06-01 12:05:13 +0630

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Contacts from "./pages/Contacts";
+import Create from "./pages/Create";
+import Frequent from "./pages/Frequent";
+import Person from "./pages/Person";
+import Search from "./pages/Search";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Contacts />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/frequent" element={<Frequent />} />
+      <Route path="/person/:id" element={<Person />} />
+      <Route path="/search/:id" element={<Search />} />
     </Routes>
   );
 };
